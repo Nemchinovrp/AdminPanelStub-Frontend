c2edf81b47201d2cf4b781d1757ca3f192b310f3
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-10-12 16:05:08 +0300

    work with service

---
+++ src/app/components/user/categories/categories.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from '../../../services/data-handler.service';
+import {Category} from '../../../model/Category';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent implements OnInit {
+  categories: Category[];
+
+  constructor(private dataHandler: DataHandlerService) {
+  }
+
+  ngOnInit() {
+    this.categories = this.dataHandler.getCategories();
+    console.log(this.categories);
+  }
+}
